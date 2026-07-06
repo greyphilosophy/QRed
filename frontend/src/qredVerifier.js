6ebbb843e7cab0ba3360b756dec81e80d256f5b3
greyphilosophy <greyphilosophy@gmail.com>
2026-07-05 19:33:55 -0700

    fix: standalone test.html hidden payload decode

--- frontend/src/qredVerifier.js
+++ frontend/src/qredVerifier.js
@@ -1,4 +1,7 @@
 import { verifyAsync as verifyEd25519 } from "@noble/ed25519";
+import Utils from "qrcode/lib/core/utils";
+import ECCode from "qrcode/lib/core/error-correction-code";
+import ECLevel from "qrcode/lib/core/error-correction-level";
 import { decodeB45ish } from "./textRecipes.js";
 
 export const VISIBLE_QR_TEXT = "QRED.ORG";
@@ -432,28 +435,14 @@ function codewordsFromMatrix(matrix, version) {
   return { codewords: new Uint8Array(bytes), ecLevelBits: formatInfo.ecLevelBits };
 }
 
-
-const TOTAL_CODEWORDS = [0, 26, 44, 70, 100, 134, 172, 196, 242, 292, 346, 404, 466, 532, 581, 655, 733, 815, 901, 991, 1085, 1156, 1258, 1364, 1474, 1588, 1706, 1828, 1921, 2051, 2185, 2323, 2465, 2611, 2761, 2876, 3034, 3196, 3362, 3532, 3706];
-const EC_BLOCKS_BY_LEVEL = [
-  [0, 1, 1, 1, 2, 2, 4, 4, 4, 5, 5, 5, 8, 9, 9, 10, 10, 11, 13, 14, 16, 17, 17, 18, 20, 21, 23, 25, 26, 28, 29, 31, 33, 35, 37, 38, 40, 43, 45, 47, 49],
-  [0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 4, 4, 6, 6, 6, 6, 7, 8, 8, 9, 9, 10, 12, 12, 12, 13, 14, 15, 16, 17, 18, 19, 19, 20, 21, 22, 24, 25],
-  [0, 1, 1, 2, 4, 4, 4, 5, 6, 6, 8, 8, 11, 11, 16, 16, 18, 16, 19, 21, 25, 25, 25, 34, 30, 32, 35, 37, 40, 42, 45, 48, 51, 54, 57, 60, 63, 66, 70, 74, 77],
-  [0, 1, 1, 2, 2, 4, 4, 6, 6, 8, 8, 8, 10, 12, 16, 12, 17, 16, 18, 21, 20, 23, 23, 25, 27, 29, 34, 34, 34, 35, 38, 40, 43, 45, 48, 51, 53, 56, 59, 62, 65],
-];
-const EC_CODEWORDS_BY_LEVEL = [
-  [0, 10, 16, 26, 36, 48, 64, 72, 88, 110, 130, 150, 176, 198, 216, 240, 280, 308, 338, 364, 416, 442, 476, 504, 560, 588, 644, 700, 728, 784, 812, 868, 924, 980, 1036, 1064, 1120, 1204, 1260, 1316, 1372],
-  [0, 7, 10, 15, 20, 26, 36, 40, 48, 60, 72, 80, 96, 104, 120, 132, 144, 168, 180, 196, 224, 224, 252, 270, 300, 312, 336, 360, 390, 420, 450, 480, 510, 540, 570, 570, 600, 630, 660, 720, 750],
-  [0, 17, 28, 44, 64, 88, 112, 130, 156, 192, 224, 264, 308, 352, 384, 432, 480, 532, 588, 650, 700, 750, 816, 900, 960, 1050, 1110, 1200, 1260, 1350, 1440, 1530, 1620, 1710, 1800, 1890, 1980, 2100, 2220, 2310, 2430],
-  [0, 13, 22, 36, 52, 72, 96, 108, 132, 160, 192, 224, 260, 288, 320, 360, 408, 448, 504, 546, 600, 644, 690, 750, 810, 870, 952, 1020, 1050, 1140, 1200, 1290, 1350, 1440, 1530, 1590, 1680, 1770, 1860, 1950, 2040],
-];
-
 function deinterleaveDataCodewords(interleaved, version, ecLevelBits = 0) {
-  const totalCodewords = TOTAL_CODEWORDS[version];
-  const ecTotalCodewords = EC_CODEWORDS_BY_LEVEL[ecLevelBits]?.[version];
-  const ecTotalBlocks = EC_BLOCKS_BY_LEVEL[ecLevelBits]?.[version];
+  const totalCodewords = Utils.getSymbolTotalCodewords(version);
+  const ecLevel = [ECLevel.M, ECLevel.L, ECLevel.H, ECLevel.Q][ecLevelBits] || ECLevel.M;
+  const ecTotalCodewords = ECCode.getTotalCodewordsCount(version, ecLevel);
+  const ecTotalBlocks = ECCode.getBlocksCount(version, ecLevel);
   if (!totalCodewords || !ecTotalCodewords || interleaved.length < totalCodewords || !ecTotalBlocks) return interleaved;
   const dataTotalCodewords = totalCodewords - ecTotalCodewords;
-  const blocksInGroup2 = dataTotalCodewords % ecTotalBlocks;
+  const blocksInGroup2 = totalCodewords % ecTotalBlocks;
   const blocksInGroup1 = ecTotalBlocks - blocksInGroup2;
   const dataCodewordsInGroup1 = Math.floor(dataTotalCodewords / ecTotalBlocks);
   const dataCodewordsInGroup2 = dataCodewordsInGroup1 + 1;
@@ -572,3 +561,5 @@ export function extractHiddenQRedPayloadFromImage(imageData, width, height, scan
   }
   return bestPayload;
 }
+
+export { sampleQrMatrix, codewordsFromMatrix, deinterleaveDataCodewords, hiddenPayloadByteOffset };
